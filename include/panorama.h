0de18d481e94027910c1c2333a8232683f6ba9f3
janiehnrd <58337469+janiehnrd@users.noreply.github.com>
2021-06-17 17:14:43 +0200

    Update panorama.h

--- include/panorama.h
+++ include/panorama.h
@@ -12,12 +12,16 @@ void panorama(string directory)
 {
     vector<Mat> imgs;
 
-    vector<cv::String> filenames = { "half1.jpg","half2.jpg" };
+   vector<string> filenames;
+    string path = directory + "/*.jpg";
+    glob(path, filenames, false);
 
-
-    for (auto & filename : filenames)
+    vector<string>::iterator it;
+    for (it = filenames.begin(); it != filenames.end(); ++it)
     {
-        imgs.push_back(imread(samples::findFile(filename)));
+        Mat new_img;
+        new_img = imread(*it, IMREAD_COLOR);
+        imgs.push_back(new_img);
     }
 
 
@@ -29,11 +33,11 @@ void panorama(string directory)
 
     string result_img ("panoramized_image.png");
 
-    imwrite(result_img, pano);
-
     namedWindow("New Image", WINDOW_AUTOSIZE);
     imshow("New Image", pano);
     waitKey();
+    
+    imwrite(result_img, pano);
 
 }
 
