dfe3e5d7ebb2880d23373cd3abd670338f49d5d8
Marceaugerardin <59610022+Marceaugerardin@users.noreply.github.com>
2021-06-17 12:33:25 +0200

    Modification de l'erosion

--- include/dilataion_erosion.h
+++ include/dilataion_erosion.h
@@ -1,62 +1,28 @@
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-#include <iostream>
-using namespace cv;
-using namespace std;
-Mat src, erosion_dst, dilation_dst;
-int erosion_elem = 0;
-int erosion_size = 0;
-int dilation_elem = 0;
-int dilation_size = 0;
-int const max_elem = 2;
-int const max_kernel_size = 21;
-void Erosion(int, void*);
-void Dilation(int, void*);
-int dilatation_erosion(string filename)
-{
-    src = imread(filename, IMREAD_COLOR);
 
-    namedWindow("Erosion Demo", WINDOW_GUI_EXPANDED);
-    namedWindow("Dilation Demo", WINDOW_GUI_EXPANDED);
-    moveWindow("Dilation Demo", src.cols, 0);
-    createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
-        &erosion_elem, max_elem,
-        Erosion);
-    createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
-        &erosion_size, max_kernel_size,
-        Erosion);
-    createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Dilation Demo",
-        &dilation_elem, max_elem,
-        Dilation);
-    createTrackbar("Kernel size:\n 2n +1", "Dilation Demo",
-        &dilation_size, max_kernel_size,
-        Dilation);
-    Erosion(0, 0);
-    Dilation(0, 0);
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+int dilatation_erosion(string filename, int size) {
+    Mat image = imread(filename, IMREAD_COLOR);
+    Mat erodedimage;
+    erode(image, erodedimage, getStructuringElement(MORPH_RECT, Size(size, size)));
+    String window_name = "Photo non-modifiée";
+    String window_name_erodedimage = "Image érodée";
+    namedWindow(window_name);
+    namedWindow(window_name_erodedimage);
+    imshow(window_name, image);
+    imshow(window_name_erodedimage, erodedimage);
+    
+
+    Mat dilatedimage;
+    dilate(image, dilatedimage, getStructuringElement(MORPH_RECT, Size(size, size)));
+    String window_name_dilatedimage = "Image dilatée";
+    namedWindow(window_name_erodedimage);
+    imshow(window_name_dilatedimage, dilatedimage);
+
     waitKey(0);
+    destroyAllWindows();
     return 0;
-}
-void Erosion(int, void*)
-{
-    int erosion_type = 0;
-    if (erosion_elem == 0) { erosion_type = MORPH_RECT; }
-    else if (erosion_elem == 1) { erosion_type = MORPH_CROSS; }
-    else if (erosion_elem == 2) { erosion_type = MORPH_ELLIPSE; }
-    Mat element = getStructuringElement(erosion_type,
-        Size(2 * erosion_size + 1, 2 * erosion_size + 1),
-        Point(erosion_size, erosion_size));
-    erode(src, erosion_dst, element);
-    imshow("Erosion Demo", erosion_dst);
-}
-void Dilation(int, void*)
-{
-    int dilation_type = 0;
-    if (dilation_elem == 0) { dilation_type = MORPH_RECT; }
-    else if (dilation_elem == 1) { dilation_type = MORPH_CROSS; }
-    else if (dilation_elem == 2) { dilation_type = MORPH_ELLIPSE; }
-    Mat element = getStructuringElement(dilation_type,
-        Size(2 * dilation_size + 1, 2 * dilation_size + 1),
-        Point(dilation_size, dilation_size));
-    dilate(src, dilation_dst, element);
-    imshow("Dilation Demo", dilation_dst);
 }
