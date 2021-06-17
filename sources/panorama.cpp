1f82dc3e1632f4b90737ba8671efbd89770c4813
janiehnrd <58337469+janiehnrd@users.noreply.github.com>
2021-06-17 17:16:12 +0200

    Add files via upload

---
+++ sources/panorama.cpp
@@ -0,0 +1,42 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/stitching.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include<iostream>
+#include <string>
+#include <filesystem>
+using namespace std;
+using namespace cv;
+
+void panorama(string directory)
+{
+    vector<Mat> imgs;
+
+    vector<string> filenames;
+    string path = directory + "/*.jpg";
+    glob(path, filenames, false);
+
+    vector<string>::iterator it;
+    for (it = filenames.begin(); it != filenames.end(); ++it)
+    {
+        Mat new_img;
+        new_img = imread(*it, IMREAD_COLOR);
+        imgs.push_back(new_img);
+    }
+
+
+    Stitcher::Mode mode = Stitcher::PANORAMA;
+
+    Mat pano;
+    Ptr<Stitcher> stitcher = Stitcher::create(mode);
+    Stitcher::Status status = stitcher->stitch(imgs, pano);
+
+    string result_img = "panoramized_image.png";
+
+    namedWindow("New Image", WINDOW_AUTOSIZE);
+    imshow("New Image", pano);
+    waitKey();
+
+    imwrite(result_img, pano);
+
+}
---
+++ sources/resize.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/stitching.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include<iostream>
+#include <string>
+#include <filesystem>
+using namespace std;
+using namespace cv;
+
+
+void resizing(string filename, double fx, double fy)
+{
+    Mat img = cv::imread(filename, IMREAD_COLOR);
+    Mat newimg;
+
+    resize(img, newimg, (newimg).size(), fx, fy,INTER_LINEAR);
+
+    imwrite("resized_image.jpg", newimg);
+
+    namedWindow("Original Image", WINDOW_AUTOSIZE);
+    namedWindow("New Image", WINDOW_AUTOSIZE);
+    imshow("Original Image", img);
+    imshow("New Image", newimg);
+    waitKey();
+
+}
+
